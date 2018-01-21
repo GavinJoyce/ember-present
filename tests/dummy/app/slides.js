acebafe89c2f36ddf34fa3f24979f152da173078
Gavin Joyce <gavinjoyce@gmail.com>
2018-01-21 10:31:04 +0000

    remove unused config

--- tests/dummy/app/slides.js
+++ tests/dummy/app/slides.js
@@ -9,15 +9,7 @@ export default {
   slides: [
     { name: 'slide-1' },
     { name: 'slide-2' },
-    {
-      name: 'slide-3',
-      roles: {
-        screen: {
-          type: 'video',
-          src: 'http://vs-mobile-client.gavinjoyce.com.s3-website-eu-west-1.amazonaws.com/videos/final_intro.mp4'
-        }
-      }
-    },
+    { name: 'slide-3' },
     { name: 'slide-4' },
   ]
 }
