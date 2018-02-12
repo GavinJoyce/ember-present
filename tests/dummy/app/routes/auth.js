97527803669c06cbe7a1377319c41785d1a98c38
Gavin Joyce <gavinjoyce@gmail.com>
2018-02-12 19:17:26 +0000

    go to slide on login

--- tests/dummy/app/routes/auth.js
+++ tests/dummy/app/routes/auth.js
@@ -2,7 +2,20 @@ import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 
 export default Route.extend({
+  slides: inject(),
   session: inject(),
+
+  queryParams: {
+    slide: {
+      replace: true
+    }
+  },
+
+  beforeModel({ queryParams }) {
+    if (queryParams.slide) {
+      this.get('slides').goToSlide(queryParams.slide);
+    }
+  },
 
   activate() {
     this._super(...arguments);
