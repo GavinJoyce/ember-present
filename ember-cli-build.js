c709250bfd1b0add8f053c3251c3fffbeef66ff0
Gavin Joyce <gavinjoyce@gmail.com>
2018-07-15 11:32:25 +0100

    upgrade ember-cli-tailwind

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    'ember-cli-tailwind': {
-      buildTarget: 'dummy',
-    },
     snippetPaths: ['tests/dummy/app/snippets']
   });
 
