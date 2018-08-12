85f916dedfb599f5e32df7c35b32c4c80f898e4a
Gavin Joyce <gavinjoyce@gmail.com>
2018-08-12 11:30:04 +0100

    add simple button component

--- addon/mixins/realtime.js
+++ addon/mixins/realtime.js
@@ -12,16 +12,6 @@ export default Mixin.create(DisposableMixin, {
     this.registerDisposable(() => realtime.off(name, callback));
   },
 
-  async addMetadataSummaryRealtimeListener(key, callback) {
-    this.addRealtimeListener(`users.metadata.${key}.summary`, callback);
-
-    let realtime = this.get('realtime');
-    let data = await realtime.emitWithResponse('getMetadataSummary', key);
-
-    if (this.isDestroyed) { return; }
-    callback(data);
-  },
-
   async addMetadataCountsRealtimeListener(key, callback) {
     this.addRealtimeListener(`users.metadata.${key}.counts`, callback);
 
